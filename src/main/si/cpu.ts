05b97e0d9027153efa6e875e25abbae7d4f485b4
Mario Lazzari <mario.lazzari@gmail.com>
2022-12-28 22:50:18 +0100

    updated libs

--- src/main/si/cpu.ts
+++ src/main/si/cpu.ts
@@ -1,14 +1,36 @@
-import si from 'systeminformation';
-import { IpcMainEvent } from 'electron';
+import si from "systeminformation";
+import { IpcMainEvent } from "electron";
 
 const onCpu = async (e: IpcMainEvent) => {
   try {
-    const res = await Promise.all([si.cpu(), si.cpuTemperature()]);
-    const data = { cpu: res[0], temperatures: res[1] };
-    e.reply('cpu', data);
+    const res = await Promise.all([
+      si.cpu(),
+      si.cpuFlags(),
+      si.cpuCache(),
+      si.cpuCurrentSpeed(),
+      si.cpuTemperature(),
+    ]);
+    const data = {
+      cpu: res[0],
+      flags: res[1],
+      cache: res[2],
+      speed: res[3],
+      temperatures: res[4],
+    };
+    e.reply("cpu", data);
   } catch (ex) {
-    const error = ex instanceof Error ? ex.message : 'Error reading cpu info';
-    e.reply('cpu', error);
+    const error = ex instanceof Error ? ex.message : "Error reading cpu info";
+    e.reply("cpu", error);
+  }
+};
+
+export const onCpuSpeed = async (e: IpcMainEvent) => {
+  try {
+    const res = await si.cpuCurrentSpeed();
+    e.reply("cpuSpeed", res);
+  } catch (ex) {
+    const error = ex instanceof Error ? ex.message : "Error reading cpu speed";
+    e.reply("cpuSpeed", error);
   }
 };
 
